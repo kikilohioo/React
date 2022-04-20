7e5b4d13eb371532f6c9058ed40362f8827c1a67
Caiqui Viera <cvcommunitymanager@gmail.com>
2022-04-20 17:43:44 -0300

    start hooks projects

---
+++ 05-hook-app/src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import './effects.css'
+
+export const SimpleForm = () => {
+
+	const [formState, setFormState] = useState({
+		name: '',
+		email: ''
+	})
+
+	const { name, email } = formState;
+
+	useEffect(() => {
+		console.log("hey")
+	}, [])
+
+	const handdleInputChange = ({target}) => {
+		setFormState({ //siempre que el state sea un objeto o array, hay que desestructurarlo tambien al intentar setearlo
+			...formState,
+			[target.name]: target.value
+		})
+	}
+  return (
+	<>
+		<h1>useEffect</h1>
+		<hr />
+		  <div className='form-group'>
+			  <input
+				  type='text'
+				  name='name'
+				  className='form-control'
+				  placeholder='Ingresa tu nombre'
+				  autoComplete='off'
+				  value={name}
+				  onChange={handdleInputChange}
+			  />
+		  </div>
+		  <br/>
+		<div className='form-group'>
+			  <input
+				  type='text'
+				  name='email'
+				  className='form-control'
+				  placeholder='Ingresa tu nombre'
+				  autoComplete='off'
+				  value={email}
+				  onChange={handdleInputChange}
+			  />
+		</div>
+
+	</>
+  )
+}
--- 05-hook-app/src/index.js
+++ 05-hook-app/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { HookApp } from './HookApp';
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
+
 
 ReactDOM.render(
-  <CounterApp />,
+  <SimpleForm />,
   document.getElementById('root')
 );
 
